5d248480b8a284e31ccfd9aad988ad5888a21d53
YuKJDev <yury.a.kropotkin@gmail.com>
2021-11-20 01:18:23 +1100

    uploading project files

---
+++ android/src/ru/gb/yukjdev/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package ru.gb.yukjdev;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import ru.gb.yukjdev.StarGameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new StarGameMain(), config);
+    }
+}

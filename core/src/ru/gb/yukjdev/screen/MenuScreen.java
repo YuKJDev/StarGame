273890659b3a1339c76a3d891b711cdeaebe3ba1
YuKJDev <yury.a.kropotkin@gmail.com>
2021-11-25 00:40:58 +1100

    получаю предупреждение библиотека Lwjgl в системе установлена: WARNING: An illegal reflective access operation has occurred WARNING: Illegal reflective access by org.lwjgl.LWJGLUtil$3 (file:/home/yury/IdeaProjects/libGDX/StarGame/gradle/caches/modules-2/files-2.1/org.lwjgl.lwjgl/lwjgl/2.9.3/3df168ac74e4a8c96562cdff24ad352e255bf89c/lwjgl-2.9.3.jar) to method java.lang.ClassLoader.findLibrary(java.lang.String) WARNING: Please consider reporting this to the maintainers of org.lwjgl.LWJGLUtil$3 WARNING: Use --illegal-access=warn to enable warnings of further illegal reflective access operations WARNING: All illegal access operations will be denied in a future release ALSA lib pcm_dmix.c:1089:(snd_pcm_dmix_open) unable to open slave AL lib: (EE) alsa_open_playback: Could not open playback device 'default': Нет такого файла или каталога org.lwjgl.LWJGLException: Could not locate OpenAL library. 	at org.lwjgl.openal.AL.create(AL.java:156) 	at org.lwjgl.openal.AL.create(AL.java:102) 	at org.lwjgl.openal.AL.create(AL.java:206) 	at com.badlogic.gdx.backends.lwjgl.audio.OpenALLwjglAudio.<init>(OpenALLwjglAudio.java:71) 	at com.badlogic.gdx.backends.lwjgl.LwjglApplication.createAudio(LwjglApplication.java:278) 	at com.badlogic.gdx.backends.lwjgl.LwjglApplication.<init>(LwjglApplication.java:90) 	at com.badlogic.gdx.backends.lwjgl.LwjglApplication.<init>(LwjglApplication.java:71) 	at ru.gb.yukjdev.desktop.DesktopLauncher.main(DesktopLauncher.java:12) ALSA lib pcm_dmix.c:1089:(snd_pcm_dmix_open) unable to open slave AL lib: (EE) alsa_open_playback: Could not open playback device 'default': Нет такого файла или каталога org.lwjgl.LWJGLException: Could not locate OpenAL library. 	at org.lwjgl.openal.AL.create(AL.java:156) 	at org.lwjgl.openal.AL.create(AL.java:102) 	at org.lwjgl.openal.AL.create(AL.java:206) 	at com.badlogic.gdx.backends.lwjgl.audio.OpenALLwjglAudio.<init>(OpenALLwjglAudio.java:71) 	at com.badlogic.gdx.backends.lwjgl.LwjglApplication.createAudio(LwjglApplication.java:278) 	at com.badlogic.gdx.backends.lwjgl.LwjglApplication.<init>(LwjglApplication.java:90) 	at com.badlogic.gdx.backends.lwjgl.LwjglApplication.<init>(LwjglApplication.java:71) 	at ru.gb.yukjdev.desktop.DesktopLauncher.main(DesktopLauncher.java:17) LwjglGraphics: OpenGL 3.2+ core profile (GLES 3.0) not supported. LwjglGraphics: created OpenGL 3.2+ core profile (GLES 3.0) context. This is experimental! Exception in thread "LWJGL Application" java.lang.IllegalStateException: From thread Thread[LWJGL Application,5,main]: Thread[LWJGL Application,5,main] already has the context current 	at org.lwjgl.opengl.ContextGL.checkAccess(ContextGL.java:184) 	at org.lwjgl.opengl.ContextGL.forceDestroy(ContextGL.java:241) 	at org.lwjgl.opengl.DrawableGL.destroy(DrawableGL.java:130) 	at org.lwjgl.opengl.Display$5.destroy(Display.java:834) 	at org.lwjgl.opengl.Display.destroy(Display.java:1095) 	at com.badlogic.gdx.backends.lwjgl.LwjglGraphics.createDisplayPixelFormat(LwjglGraphics.java:338) 	at com.badlogic.gdx.backends.lwjgl.LwjglGraphics.setupDisplay(LwjglGraphics.java:264) 	at com.badlogic.gdx.backends.lwjgl.LwjglApplication.mainLoop(LwjglApplication.java:145) 	at com.badlogic.gdx.backends.lwjgl.LwjglApplication$1.run(LwjglApplication.java:127)
    Также не отрисовывается лоотип, причину не могу понять

---
+++ core/src/ru/gb/yukjdev/screen/MenuScreen.java
@@ -0,0 +1,70 @@
+package ru.gb.yukjdev.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.gb.yukjdev.base.BaseScreen;
+import ru.gb.yukjdev.math.Rect;
+import ru.gb.yukjdev.splite.Background;
+
+public class MenuScreen extends BaseScreen {
+
+    private Background background;
+    private Vector2 pos;
+    private Vector2 v;
+    private Vector2 touch;
+    private Texture img;
+
+    private static final float V_LEN = 0.5f;
+
+
+    @Override
+    public void show() {
+        super.show();
+        pos = new Vector2(0, 0);
+        Texture bg = new Texture("textures/bg.png");
+        img = new Texture("badlogic.jpg");
+        background = new Background(new TextureRegion(bg));
+        v = new Vector2();
+        touch = new Vector2();
+
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        batch.begin();
+        batch.draw(img, pos.x, pos.y);
+        background.draw(batch);
+        batch.end();
+        if (touch.dst(pos) <= v.len()) {
+            pos.set(touch);
+        } else {
+            pos.add(v);
+        }
+
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        img.dispose();
+
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(pos).setLength(V_LEN));
+        return false;
+    }
+
+}
